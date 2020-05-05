3afd847178818e25aec5c4a8a9934996abf5acf2
johancarlos <johancarlos.bo@gmail.com>
2020-05-05 09:34:15 -0400

    fixed wow test

--- src/Facturador/PlanPostpago.java
+++ src/Facturador/PlanPostpago.java
@@ -5,9 +5,23 @@ public class PlanPostpago extends TarificadorInterface{
 	public PlanPostpago() {
 	
 	}
+    private static PlanPostpago planPostPago;
 
 
-		
-	
+	public static PlanPostpago getInstance() {
+        if (planPostPago == null) {
+        	planPostPago = new PlanPostpago();
+        }
+        return planPostPago;
+    }
+
+    @Override
+    public double calcularTarifa(final Llamada llamada){
+    	
+        return calcularTarifa(llamada);
+    }
+
+
+
 
 }
--- src/test/PlanWowTest.java
+++ src/test/PlanWowTest.java
@@ -31,12 +31,21 @@ class PlanWowTest {
 	
 	 @Test
 	    public void esNumeroAmigo() {
-	        setUp();
-	        RegistroPlanWow clienteWow = new RegistroPlanWow();
+	        RegistroPlanWow wowRegistro = new RegistroPlanWow();
+	        wowRegistro.setClientes(new ArrayList<>());
+	        PlanWow clienteWow = new PlanWow();
+	        clienteWow.setNumeroCliente(7717271);
+	        List<Integer> friendNumbers = new ArrayList<>();
+	        friendNumbers.add(70809010);
+	        friendNumbers.add(70809011);
+	        friendNumbers.add(70809012);
+	        friendNumbers.add(70809013);
+	        clienteWow.setNumerosAmigos(friendNumbers);
+	        wowRegistro.registroCliente(clienteWow);
 	        Llamada nuevaLlamada = new Llamada();
 	        nuevaLlamada.setNumeroOrigen(7717271);
-	        nuevaLlamada.setNumeroDestino(70809011);
-	        boolean result = clienteWow.esNumeroAmigo(nuevaLlamada);
+	        nuevaLlamada.setNumeroDestino(70809010);
+	        boolean result = wowRegistro.esNumeroAmigo(nuevaLlamada);
 	        assertTrue(result);
 	    }
 
